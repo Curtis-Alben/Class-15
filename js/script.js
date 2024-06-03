7e444656326321f71265069f64267f9dac911741
PestilenceGames <coenraadwbasson@gmail.com>
2024-06-03 12:30:42 +0200

    Main Commit

---
+++ js/script.js
@@ -0,0 +1,42 @@
+
+function setup(){
+    let currentUser = sessionStorage.getItem("user");
+    console.log(currentUser);
+}
+
+let form = document.forms["my-form"];
+form.addEventListener("submit", getValues);
+
+function getValues(event){
+    event.preventDefault();
+
+    let formData = {
+        "name": this.name.value,
+        "bio": this.bio.value,
+        "fav-color": this['fav-color'].value,
+        "gender": this.gender.value,
+        "fav-food": []
+    }
+
+    let favFoods = document.querySelectorAll(".fav-foods");
+    for (let food of favFoods){
+        if(food.checked == true){
+            formData['fav-food'].push(food.value);
+        }
+    }
+
+let out = `
+    <p>Name: <span>${formData.name}</span></p>
+    <p>Bio: <span>${formData.bio}</span></p>
+    <p>Fav Color: <span>${formData['fav-colour']}</span></p>
+    <p>Gender: <span>${formData.gender}</span></p>
+    <p>Fav Food: <span>${formData['fav-food']}</span></p>
+`;
+
+document.querySelector(".out code").innerHTML = out;
+
+
+alert("Hi " + formData.name + ". Thank you for you message");
+
+sessionStorage.setItem("user", formData);
+}
